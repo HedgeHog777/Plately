22ff741182da0afd02a007c9e77b8dfc96677ae5
Mr_HedgeHog_UA <60219855+HedgeHog777@users.noreply.github.com>
2026-08-02 23:26:08 +0200

    Initialize FastAPI application with root endpoint

--- backend/app.py
+++ backend/app.py
@@ -4,3 +4,16 @@ Commit #1
 If you're reading this,
 we actually started.
 """
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Plately API",
+    version="0.0.1"
+)
+
+@app.get("/")
+def root():
+    return {
+        "project": "Plately",
+        "status": "Building..."
+    }
